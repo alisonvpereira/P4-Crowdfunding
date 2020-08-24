cbfdaadc57a93d2be36e55ab92bf92a10baccac7
alisonvpereira <alisonvpereira@gmail.com>
2020-08-24 15:35:07 +0800

    started project

---
+++ skillender/projects/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    goal = models.IntegerField()
+    image = models.URLField()
+    is_open = models.BooleanField()
+    date_created = models.DateTimeField()
+    owner = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='owner_projects'
+    )
+
+class Pledge(models.Model):
+    hours = models.IntegerField()
+    comment = models.CharField(max_length=200)
+    anonymous = models.BooleanField()
+    project = models.ForeignKey(
+        'Project',
+        on_delete=models.CASCADE,
+        related_name='pledges'
+    )
+    volunteer = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='volunteer_pledges'
+    )
+
